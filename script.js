b1a8cd0af72c249c8f1b798494dfabc57fb9fab6
nasir35 <nasir2242001@gmail.com>
2023-03-08 23:24:51 +0600

    added functionality of response validation

--- script.js
+++ script.js
@@ -9,9 +9,17 @@ const downloadBtn = document.getElementById('download-btn');
 let elements = [];
 addedElements.innerText = elements.length>0? elements : '{}';
 
+
+const require = document.getElementById('require');
+const allForms = document.querySelectorAll('form input[type="text"]');
+allForms.forEach(inp => {
+    inp.addEventListener('change', ()=>{
+        inp.classList.remove('invalid');
+    })
+})
 const addElement = () => {
     if(inputVal.value.length){
-        if(elements.indexOf(inputVal.value)===-1){
+        if(elements.indexOf(inputVal.value)===-1 && pickedItems.indexOf(inputVal.value) === -1){
             let element = inputVal.value;
             elements = [...elements, element];
             addedElements.innerText = elements;
@@ -27,9 +35,11 @@ addBtn.addEventListener("click", () => addElement());
 
 const choice = document.getElementById('choice');
 const inp1 = document.getElementById("inp1");
+const inp2 = document.getElementById("picked-item");
 choice.addEventListener("change", () => {
     if(choice.value === "individual"){
         inp1.disabled = false;
+        inp1.placeholder = 'Picked for..';
     }
     else{
         inp1.disabled = true;
@@ -44,16 +54,56 @@ const shuffle = () => {
         pickedItems = [...pickedItems, pickedItem];
         const newRow = document.createElement('tr');
         newRow.innerHTML = `
-            <td>${++count}</td>
+            <td>${choice.value === 'count' ? ++count : inp1.value}</td>
             <td>${pickedItem}</td>
         `
         pickedElements.appendChild(newRow);
         elements.splice(index, 1);
         addedElements.innerText = elements.length ? elements : '{}';
+        inp1.value = '';
+        if(count!=0){
+            inp2.disabled = true;
+            choice.disabled = true;
+        }
     }
 }
 
-shuffleBtn.addEventListener("click", ()=>shuffle());
+choice.addEventListener("change", () => {
+    if(choice.value =='count'){
+        document.getElementById('th1').innerText = 'SL No';
+    }
+    else{
+        document.getElementById('th1').innerText = 'Responsiblities';
+    }
+})
+document.getElementById('th1').innerText = 'SL No';
+document.getElementById('th2').innerText = inp2.placeholder;
+
+inp2.addEventListener('change', () => {
+    document.getElementById('th2').innerText = inp2.value.length ? inp2.value : 'Picked Item';
+})
+shuffleBtn.addEventListener("click", ()=>{
+    const shuffleBox = document.getElementById('shuffle-box');
+    const inputs = shuffleBox.querySelectorAll('form input[type="text"]');
+    console.log(inputs);
+    let flag = true;
+    for(const i of inputs){
+        if(i.disabled === false && i.value.length===0){
+            i.classList.add('invalid')
+            flag = false;
+            break;
+        }
+    }
+    if(flag){
+        shuffle(); 
+    }
+    else if(inp1.disabled===true && inp2.disabled === true){
+        shuffle();
+    }
+    else{
+        alert("Please fill out the required inputs!");
+    }
+});
 
 const download = () => {
     const newWin = window.open('');
@@ -115,6 +165,6 @@ const download = () => {
     </body>
     `
     newWin.document.write(newHtml.outerHTML);
-    newWin.print();
+    newWin.print(); 
 }
 downloadBtn.addEventListener('click', () => download());
